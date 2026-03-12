820f927bd201214cdeac7002c9e5a57a58da2f64
gmcbr7 <mctevogigio@gmail.com>
2026-03-12 16:40:11 -0300

    modified:   Matcher.lua

--- Matcher.lua
+++ Matcher.lua
@@ -2,24 +2,71 @@
 local utils = require('./Utils')
 local match = {}
 
+local knownTypes = {
+    ["nil"] = true,
+    ["boolean"] = true,
+    ["number"] = true,
+    ["string"] = true,
+    ["table"] = true,
+    ["function"] = true,
+    ["thread"] = true,
+    ["userdata"] = true,
+    ["vector"] = true,
+    ["buffer"] = true,
+    ["Instance"] = true,
+    ["EnumItem"] = true,
+    ["Enum"] = true,
+    ["Enums"] = true,
+    ["Vector2"] = true,
+    ["Vector2int16"] = true,
+    ["Vector3"] = true,
+    ["Vector3int16"] = true,
+    ["CFrame"] = true,
+    ["Color3"] = true,
+    ["BrickColor"] = true,
+    ["UDim"] = true,
+    ["UDim2"] = true,
+    ["Rect"] = true,
+    ["Region3"] = true,
+    ["Region3int16"] = true,
+    ["NumberSequence"] = true,
+    ["ColorSequence"] = true,
+    ["NumberSequenceKeypoint"] = true,
+    ["ColorSequenceKeypoint"] = true,
+    ["NumberRange"] = true,
+    ["PhysicalProperties"] = true,
+    ["Ray"] = true,
+    ["Axes"] = true,
+    ["Faces"] = true,
+    ["Random"] = true,
+    ["DateTime"] = true,
+    ["TweenInfo"] = true,
+    ["RBXScriptSignal"] = true,
+    ["RBXScriptConnection"] = true,
+    ["PathWaypoint"] = true,
+    ["any"] = true,
+}
+
 function match.normalizeSpec(spec: any)
-	if spec == nil then
-		return {mode = "any"}
-	elseif typeof(spec) == "string" then
-		if spec == "any" then
-			return {mode = "any"}
-		else
-			return {mode = "type", type = spec, typeId = utils.getTypeId(spec)}
-		end
-	elseif typeof(spec) == "table" and spec.mode then
-		local normalized = spec
-		if normalized.mode == "type" and normalized.type then
-			normalized.typeId = utils.getTypeId(normalized.type)
-		end
-		return normalized
-	else
-		return {mode = "value", value = spec}
-	end
+    if spec == nil then
+        return {mode = "any"}
+    elseif typeof(spec) == "string" then
+        if spec == "any" then
+            return {mode = "any"}
+        elseif knownTypes[spec] then
+            return {mode = "type", type = spec, typeId = utils.getTypeId(spec)}
+        else
+            return {mode = "value", value = spec}
+        end
+    elseif typeof(spec) == "table" and spec.mode then
+        local normalized = spec
+        if normalized.mode == "type" and normalized.type then
+            normalized.typeId = utils.getTypeId(normalized.type)
+        end
+        return normalized
+    else
+        return {mode = "value", value = spec}
+    end
 end
 
 function match.matches(arg: any, spec: any, hierarchy: any): boolean
